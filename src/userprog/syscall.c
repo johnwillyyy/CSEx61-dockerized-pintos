d71586fc10f48e8fe5bfc0509b57b323b258b411
Mina Nabil <minanabil2004@gmail.com>
2025-05-04 04:11:13 +0300

    Implement basic system call dispatching framework with argument validation
    - Defined system call function signatures in syscall.h
    - Created wrapper structs (OneArg, TwoArg, ThreeArg) for passing system call arguments
    - Added placeholder implementations for system calls (halt, exit, exec, wait, etc.)
    - Implemented validate() and convert() functions to ensure user address safety
    - Added load_args() to extract up to three system call arguments from user stack
    
    Co-authored-by: johnwillyyy <johnwilliamfawzy@gmail.com>
    Co-authored-by: caroline-2 <carolinehanna510@gmail.com>
    Co-authored-by: janamohamed2304 <janamohamed2304@gmail.com>
    Co-authored-by: michael23magdy <michaelmagdymagdy@gmail.com>

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -3,6 +3,9 @@
 #include <syscall-nr.h>
 #include "threads/interrupt.h"
 #include "threads/thread.h"
+#include "threads/vaddr.h"
+
+#define ERROR -1
 
 static void syscall_handler (struct intr_frame *);
 
@@ -12,9 +15,177 @@ syscall_init (void)
   intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
 }
 
+typedef void (*SystemCall)(void*);
+
+typedef struct { void* arg1; } OneArg;
+typedef struct { void* arg1; void* arg2; } TwoArg;
+typedef struct { void* arg1; void* arg2; void* arg3; } ThreeArg; 
+
+SystemCall systemCalls[] = {
+  halt,
+  exit,
+  exec,
+  wait,
+  create,
+  remove,
+  open,
+  filesize,
+  read,
+  write,
+  seek,
+  tell,
+  close
+};
+
+// OneArg one_arg;
+// TwoArg two_arg;
+// ThreeArg three_arg;
+
+// void *args[] = {
+  
+// };
+
+void halt (void* vArgs){
+  shutdown_power_off();
+}
+
+void exit (void* vArgs){
+  OneArg* Args = (OneArg*) vArgs;
+  int status = *(int*) Args->arg1;
+  //implement
+}
+
+void exec (void* vArgs){
+  OneArg* Args = (OneArg*) vArgs;
+  tid_t pid = *(tid_t*) Args->arg1; 
+  //implement
+}
+
+void wait (void* vArgs){
+  OneArg* Args = (OneArg*) vArgs;
+  const char *cmd_line = (char*)Args->arg1; 
+  //implement
+}
+
+void create (void* vArgs){
+  TwoArg* Args = (TwoArg*) vArgs;
+  const char *file = (char*)Args->arg1;
+  unsigned initial_size = *(unsigned*)Args->arg2;
+  //implement
+}
+
+void remove (void* vArgs){
+  OneArg* Args = (OneArg*) vArgs;
+  const char *file = (char*)Args->arg1;
+  //implement
+}
+
+void open (void* vArgs){
+  OneArg* Args = (OneArg*) vArgs;
+  const char *file = (char*)Args->arg1;
+  //implement
+}
+
+void filesize (void* vArgs){
+  OneArg* Args = (OneArg*) vArgs;
+  int fd = *(int*)Args->arg1;
+  //implement
+}
+
+void read (void* vArgs){
+  ThreeArg* Args = (ThreeArg*) vArgs;
+  int fd = *(int*)Args->arg1;
+  void *buffer = Args->arg2;
+  unsigned size = *(unsigned*)Args->arg3;
+  //implement
+}
+
+void write (void* vArgs){
+  ThreeArg* Args = (ThreeArg*) vArgs;
+  int fd = *(int*)Args->arg1;
+  const void *buffer = Args->arg2;
+  unsigned size = *(unsigned*)Args->arg3;
+  //implement
+}
+
+void seek (void* vArgs){
+  TwoArg* Args = (TwoArg*) vArgs;
+  int fd = *(int *)Args->arg1;
+  unsigned position = *(unsigned *)Args->arg2;
+  //implement
+}
+
+void tell (void* vArgs){
+  OneArg* Args = (OneArg*) vArgs;
+  int fd = *(int*)Args->arg1;
+  //implement
+}
+
+void close (void* vArgs){
+  OneArg* Args = (OneArg*) vArgs;
+  int fd = *(int *)Args->arg1;
+  // implement
+}
+
+void * 
+validate(void *address){
+  if(address < (void *) 0x08048000 || address >= PHYS_BASE){
+    exit(ERROR);
+  }
+  return address;
+}
+
+void *
+convert(void *address){
+  void *vaddress = pagedir_get_page(thread_current()->pagedir, address);
+  if(vaddress == NULL){
+    exit(ERROR);
+  }
+  return vaddress;
+}
+
+void * load_args(void *esp){
+  ThreeArg *Args = malloc(sizeof(ThreeArg));
+  Args->arg1 = convert(validate(esp));
+  Args->arg2 = convert(validate((void *)((int *)esp + 1)));
+  Args->arg3 = convert(validate((void *)((int *)esp + 2)));
+  return (void *) Args;
+  // switch (call_code) {
+  //   case SYS_HALT:
+  //   case SYS_EXIT:
+  //   case SYS_EXEC:
+  //   case SYS_WAIT:
+  //   case SYS_REMOVE:
+  //   case SYS_OPEN:
+  //   case SYS_FILESIZE:
+  //   case SYS_TELL:
+  //   case SYS_CLOSE:
+  //     OneArg *oneArg = malloc(sizeof(OneArg));
+  //     oneArg->arg1 = convert(validate(esp));
+  //     return (void *) oneArg;
+  //   case SYS_CREATE:
+  //   case SYS_SEEK:
+  //     TwoArg *twoArg = malloc(sizeof(TwoArg));
+  //     twoArg->arg1 = convert(validate(esp));
+  //     twoArg->arg2 = convert(validate((void *)((int *)esp + 1)));
+  //     return (void *) twoArg;
+  //   case SYS_READ:
+  //   case SYS_WRITE:
+  //     ThreeArg *threeArg = malloc(sizeof(ThreeArg));
+  //     threeArg->arg1 = convert(validate(esp));
+  //     threeArg->arg2 = convert(validate((void *)((int *)esp + 1)));
+  //     threeArg->arg3 = convert(validate((void *)((int *)esp + 2)));
+  //     return (void *) threeArg;
+  // }
+  // return NULL;
+}
+
 static void
 syscall_handler (struct intr_frame *f UNUSED) 
 {
-  printf ("system call!\n");
-  thread_exit ();
+  int call_code = *(int *)f->esp;
+  // get args
+  void* args = load_args(f->esp + 1);
+
+  systemCalls[call_code](args);
 }
--- src/userprog/syscall.h
+++ src/userprog/syscall.h
@@ -3,4 +3,19 @@
 
 void syscall_init (void);
 
+/* System Calls Definetions*/
+void halt (void* vArgs);
+void exit (void* vArgs);
+void exec (void* vArgs);
+void wait (void* vArgs);
+void create (void* vArgs);
+void remove (void* vArgs);
+void open (void* vArgs);
+void filesize (void* vArgs);
+void read (void* vArgs);
+void write (void* vArgs);
+void seek (void* vArgs);
+void tell (void* vArgs);
+void close (void* vArgs);
+
 #endif /* userprog/syscall.h */
